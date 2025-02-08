699e963f024bf586e6ec5109091df37610b71f26
Pierre Lasante <pierre.lasante@videotron.ca>
2025-02-08 10:14:04 -0500

    signup avec async/await est plus lisible.

--- controllers/users.js
+++ controllers/users.js
@@ -4,12 +4,13 @@ exports.sayHi = (req, res) => {
   res.json({message: 'Hello Json!'});
 }
 
-exports.signUp = (req, res) => {
+exports.signUp = async (req, res) => {
   //console.log('req.body', req.body);
   const user = new User(req.body);
-  user.save().then(savedUser => {
+  try {
+    const savedUser = await user.save();
     res.json({ savedUser });
-  }).catch(err => {
+  } catch (err) {
     return res.status(400).json({ message: err });
-  });
+  }
 };
