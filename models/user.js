71b636a34790b036f9390430ff7c918dac4d8afc
Pierre Lasante <pierre.lasante@videotron.ca>
2025-02-08 09:21:53 -0500

    Create User model

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require('uuid/v1');
+const crypto = require("node:crypto");
+const crypto = require("node:crypto");
+
+const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+    },
+    hashedPassword: {
+      type: String,
+      required: true
+    },
+    about: {
+      type: String,
+      trim: true
+    },
+    salt: String,
+    role: {
+      type: {
+        type: Number,
+        default: 0
+      }
+    },
+    history: {
+      type: Array,
+      default: []
+    },
+  }, {timestamps: true}
+);
+
+// Virtual fields
+userSchema.virtual('password')
+.set(function (password) {
+  this.password = password;
+  this.salt = uuidv1();
+  this.hashedPassword = this.encryptPassword(password);
+})
+.get(function () {
+  return this._password
+})
+
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if(!password) return '';
+    try {
+      return crypto.createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch(err) {
+      return '';
+    }
+  }
+}
+
+module.exports = mongoose.model("User", userSchema);
