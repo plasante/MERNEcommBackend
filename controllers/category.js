7dda1be3f6c46f0b3bb0082de4d8477f99990c34
Pierre Lasante <pierre.lasante@videotron.ca>
2025-02-23 09:00:47 -0500

    Finished update delete list all category

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,6 @@
 const Category = require('../models/category');
-const { errorHandler } = require('../helpers/DbErrorHandler');
-const res = require("express/lib/response");
-const {next} = require("lodash/seq");
+const { errorHandler} = require('../helpers/DbErrorHandler');
+const Product = require("../models/product");
 
 exports.read = (req, res) => {
   return res.json(req.category);
@@ -30,4 +29,43 @@ exports.create = async (req, res) => {
       error: errorHandler(err)
     });
   }
+}
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const category = req.category;
+  category.name = req.body.name;
+  try {
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err)
+    });
+  }
+}
+
+exports.remove = async (req, res) => {
+  const category = req.category;
+
+  if (!category) {
+    return res.status(400).json({
+      error: "Category not found",
+    });
+  }
+
+  try {
+    // Use the Model's deleteOne method
+    await Category.deleteOne({ _id: category._id });
+    return res.json({ message: "Category deleted successfully" });
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
+};
+
+exports.list = async (req, res) => {
+  const categories = await Category.find();
+  res.json(categories);
 }
--- routes/category.js
+++ routes/category.js
@@ -8,12 +8,14 @@ const {
 const { userById } = require('../controllers/user');
 
 const {
-  create, categoryById, read
+  create, categoryById, read, update, remove, list
 } = require('../controllers/category');
 
 router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
-
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.get('/categories', list);
 
 // We run categoryById middleware when categoryId is not null
 // req.category will be add to the request object
