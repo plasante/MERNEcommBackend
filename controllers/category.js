4b773f83c8d9343c3e7b7922d2aa912b82c68c86
Pierre Lasante <pierre.lasante@videotron.ca>
2025-02-23 08:07:54 -0500

    Read a category

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,23 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/DbErrorHandler');
+const res = require("express/lib/response");
+const {next} = require("lodash/seq");
+
+exports.read = (req, res) => {
+  return res.json(req.category);
+}
+
+exports.categoryById = async (req, res, next, id) => {
+  try {
+    const category = await Category.findById(id);
+    req.category = category;
+    next();
+  } catch(err) {
+    return res.status(400).json({
+      error: "Category not found"
+    });
+  }
+}
 
 exports.create = async (req, res) => {
   const newCategory = new Category(req.body);
--- routes/category.js
+++ routes/category.js
@@ -8,10 +8,16 @@ const {
 const { userById } = require('../controllers/user');
 
 const {
-  create
+  create, categoryById, read
 } = require('../controllers/category');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+
+// We run categoryById middleware when categoryId is not null
+// req.category will be add to the request object
+router.param('categoryId', categoryById);
 
 router.param('userId', userById);
 
